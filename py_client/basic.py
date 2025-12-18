49c8af7b1e2bb455651e14cd30bbc5b23f5be7df
Abass Koyang Ahmad <abasskoyang12345@gmail.com>
2025-12-18 13:46:56 +0100

    Initial commit

---
+++ py_client/basic.py
@@ -0,0 +1,8 @@
+import requests
+
+endpoint = "http://localhost:8000/api/"
+
+response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello world!"});
+print(response.json())
+# print(response.text)
+# print(response.status_code)
